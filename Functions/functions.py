0a8955faf26a9473c4fa517bf9c64f86c1757196
jmccallister93 <jaredmccallister93@gmail.com>
2022-01-28 11:04:51 -0800

    Update functions.py

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,3 +1,25 @@
+#Visualize images
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+
+def view_random_image(target_dir, target_class):
+  #Setup target directory
+  target_folder = target_dir+target_class
+
+  #Get random image
+  random_image = random.sample(os.listdir(target_folder), 1)
+  print(random_image)
+
+  #read in image and plot
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+
+  print(f"Image shape: {img.shape}")
+
+  return img
 
 import tensorflow as tf
 
